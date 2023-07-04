f976e6b563ce8e3e2c13428336feec41cd43decc
christhecoolguy <christopherdiggs1@gmail.com>
2023-07-03 17:05:05 -0700

    console

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,73 +1,106 @@
-"""New engine DBStorage: (models/engine/db_storage.py)"""
+#!/usr/bin/python3
+'''This modules defines DBStorage class'''
 
-import os
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from models.base_model import Base
+from models.base_model import BaseModel, Base
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker, scoped_session
+from os import getenv
 
 
 class DBStorage:
-    """private class attributes"""
+    '''Defining class Storage'''
+
     __engine = None
     __session = None
-    
-    """the all important __init__ *bows head*"""
-    def __init__(self):
-        """link to the user, data retrieved from environment variables"""
-        user = os.getenv('HBNB_MYSQL_USER')
-        password = os.getenv('HBNB_MYSQL_PWD')
-        host = os.getenv('HBNB_MYSQL_HOST', 'localhost')
-        database = os.getenv('HBNB_MYSQL_DB')
-        """just in case we need it"""
-        env = os.getenv('HBNB_ENV')
-        
-        """
-       A ENGINE! data grabbed from above 5 lines
-        """
-        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
-                                       .format(user, password, host, database),
-                                       pool_pre_ping=True)
-        
-        """drop all demtables if HBNB_ENV is 'test'"""
-        if env == 'test':
-            Base.metadata.drop_all(bind=self.__engine)
-        
-        """create tables pardner"""
-        Base.metadata.create_all(bind=self.__engine)
-        
-        """ssSession sinking!"""
-        Session = sessionmaker(bind=self.__engine, expire_on_commit=False)
-        self.__session = Session()
-        
-    def all(self, cls=None):
-        from models import classes
-        
-        objects = {}
-        if cls:
-            query = self.__session.query(classes[cls])
-            for obj in query.all():
-                key = '{}.{}'.format(obj.__class__.__name__, obj.id)
-                objects[key] = obj
-              
-        else:
-            for cls in classes.values():
-                query = self.__session.query(cls)
-                for obj in query.all():
-                    key = '{}.{}'.format(obj.__class__.__name__, obj.id)
-                    objects[key] = obj
-        return objects
 
-    """The easy stuff"""
+    def __init__(self):
+        '''Init for class DBstorage'''
+        USER = getenv('HBNB_MYSQL_USER')
+        PWD = getenv('HBNB_MYSQL_PWD')
+        HOST = getenv('HBNB_MYSQL_HOST')
+        DB = getenv('HBNB_MYSQL_DB')
+        ENV = getenv('HBNB_ENV')
+
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format(
+            USER, PWD, HOST, DB, ENV), pool_pre_ping=True)
+
+        if ENV == 'test':
+            Base.metadata.drop_all(self.__engine)
+
+    def all(self, cls=None):
+        """Method to retrieve all objects depending of the class name"""
+        from models.user import User
+        from models.place import Place
+        from models.city import City
+        from models.base_model import BaseModel
+        from models.state import State
+        from models.review import Review
+        from models.amenity import Amenity
+
+        classes = {
+            'BaseModel': BaseModel, 'User': User, 'Place': Place,
+            'State': State, 'City': City, 'Amenity': Amenity,
+            'Review': Review
+        }
+        if cls:
+            newDict = {}
+            allClassObjs = self.__session.query(cls).all()
+            for obj in allClassObjs:
+                key = type(obj).__name__ + "." + obj.id
+                newDict[key] = obj
+            return (newDict)
+        else:
+            allDicts = {}
+
+            dictList = []
+            State = self.all('State')
+            City = self.all('City')
+            Place = self.all('Place')
+            Review = self.all('Review')
+            User = self.all('User')
+            Amenity = self.all('Amenity')
+            dictList.append(State)
+            dictList.append(City)
+            dictList.append(Place)
+            dictList.append(Review)
+            dictList.append(User)
+            dictList.append(Amenity)
+
+            for dicts in dictList:
+                allDicts.update(dicts)
+
+            return(allDicts)
+
     def new(self, obj):
+        '''new obj for sql'''
         self.__session.add(obj)
-    
+
     def save(self):
+        '''saves through commit to db'''
         self.__session.commit()
-    
+
     def delete(self, obj=None):
+        '''deletes an obj'''
         if obj:
             self.__session.delete(obj)
-    
+
     def reload(self):
-        Session = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        from models.user import User
+        from models.place import Place
+        from models.city import City
+        from models.base_model import BaseModel
+        from models.state import State
+        from models.review import Review
+        from models.amenity import Amenity
+        '''reloads db'''
+        Base.metadata.create_all(self.__engine)
+
+        sessionFactory = sessionmaker(
+            bind=self.__engine, expire_on_commit=False)
+
+        Session = scoped_session(sessionFactory)
         self.__session = Session()
+
+    def close(self):
+        '''close method for session'''
+        self.__session.close()
